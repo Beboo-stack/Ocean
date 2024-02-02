7744f2beb762e3f2a780de51f8c65799a91c92c4
Beboo-stack <81274380+Beboo-stack@users.noreply.github.com>
2024-02-02 03:22:10 +0200

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -47,8 +47,6 @@ export default function Home() {
                     1500,
                     "We Love Peter ❤︎.",
                     1500,
-"A7la Mesa 3la Elnas Elkwayesa",
-1500,
                   ]}
                   wrapper="span"
                   speed={50}
